eeec77c07cb8b97bbf40c1eb63c51b02d36359b8
serafss2 <fares.bessrour@protonmail.com>
2019-02-20 21:32:22 -0500

    First init
    Using OpenWeather API to get the weather from my phone
    
    All coded from my android phone, need to clean a bit the structure of the responses.

---
+++ app.py
@@ -0,0 +1,33 @@
+import configparser
+import requests
+import sys
+
+
+class Config:
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('api_key.ini')
+        self.api_key = config['OpenWeather']['Key']
+
+
+class Weather:
+
+    def __init__(self):
+        self.api_key = Config().api_key
+        
+    def weather(self, location='montreal'):
+        req = requests.get('http://api.openweathermap.org/data/2.5/weather?q=montreal,ca&APPID={self.api_key}')
+        return req.content
+
+
+def main():
+    assert len(sys.argv) >= 2, 'Please enter a command'
+    weather = Weather()
+    print(weather.weather(sys.argv[1]))
+
+
+if __name__ == '__main__':
+    main()
+
+
